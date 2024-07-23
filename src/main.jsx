f5fae15a959f8ae731a35ae738a2ec8e171ed8ad
AFreborn22 <aafauzan52@gmail.com>
2024-07-23 12:04:50 +0700

    long journey

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import ReactGA from 'react-ga';
+import App from './App.jsx';
+import './index.css';
+
+const TRACKING_ID = "G-5K2KXYQ1GE";
+ReactGA.initialize(TRACKING_ID);
+ReactGA.pageview(window.location.pathname + window.location.search);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-)
+);
