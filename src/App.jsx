7f6f52385bb13fdac88dce8f66ae79e2eb8f4a17
AFreborn22 <aafauzan52@gmail.com>
2024-07-23 12:31:45 +0700

    long journey

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,6 @@
+import { useEffect } from 'react';
+import { BrowserRouter as Router, useLocation } from 'react-router-dom';
+import ReactGA from 'react-ga';
 import Navbar from './components/Navbar';
 import Carousel from './components/Carousel';
 import Product from './components/Product';
@@ -5,15 +8,35 @@ import About from './components/About';
 import Footer from './components/Footer';
 import './App.css';
 
+const TRACKING_ID = 'G-5K2KXYQ1GE'; 
+ReactGA.initialize(TRACKING_ID);
+
+function usePageViews() {
+  const location = useLocation();
+  useEffect(() => {
+    ReactGA.pageview(location.pathname + location.search);
+  }, [location]);
+}
+
 function App() {
+  usePageViews();
+
+  useEffect(() => {
+   
+    const email = email;
+    ReactGA.set({ email });
+  }, []);
+
   return (
-    <div className="App">
-      <Navbar />
-      <Carousel />
-      <Product />
-      <About />
-      <Footer />
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar />
+        <Carousel />
+        <Product />
+        <About />
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
